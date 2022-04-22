0080b96ae977142b5d9614a1ad4ffa04484ed807
Fratoni-Mateus <mateus.fratonisouza@gmail.com>
2022-04-21 22:18:58 -0300

    Javascript studies

---
+++ JavaScript Algorithms and Data Structures/Basic Javascript/nest-one-array-within-another-array.js
@@ -0,0 +1,2 @@
+// Only change code below this line
+const myArray = [["Daniel", 23],["Emma", 24]];
---
+++ JavaScript Algorithms and Data Structures/Basic Javascript/word-blanks.js
@@ -0,0 +1,8 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "Look out! A " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + " through the roof!"; // Change this line
+// Only change code above this line
