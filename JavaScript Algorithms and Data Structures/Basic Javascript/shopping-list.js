3d299b35df652efb9c2133d1b89bc311503dfd46
Mateus Fratoni <Mateus.Fratonisouza@gmail.com>
2022-10-09 20:50:06 -0300

    Continue JavaScript studies

---
+++ JavaScript Algorithms and Data Structures/Basic Javascript/shopping-list.js
@@ -0,0 +1,7 @@
+const myList = [];
+
+myList.push(["Chocolate Bar", 12]);
+myList.push(["Can of Tuna", 3]);
+myList.push(["Beer", 12]);
+myList.push(["Soy Sauce", 1]);
+myList.push(["Hot Sauce", 1]);
